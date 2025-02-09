ce4a0693748938f01f6e33ac91a3b852f073e7a8
snow-0122 <suzu1003m44@gmail.com>
2025-02-09 23:15:19 +0900

    修正点あり

--- AtlasSNS_suzunaaida/resources/views/auth/added.blade.php
+++ AtlasSNS_suzunaaida/resources/views/auth/added.blade.php
@@ -5,7 +5,9 @@
     <p>ユーザー登録が完了しました。</p>
     <p>早速ログインをしてみましょう。</p>
 
-    <p class="btn"><a href="login">ログイン画面へ</a></p>
+    <p class="btn">
+      <a href="login">ログイン画面へ</a>
+    </p>
   </div>
 
 
